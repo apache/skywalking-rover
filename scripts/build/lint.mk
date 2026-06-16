16b234df5e0668f250df6c51e222012e50b54903
kezhenxu94 <kezhenxu94@apache.org>
2026-06-16 19:37:29 +0800

    chore(ci): bump golangci-lint to v2.12.2 for go 1.26 support

--- scripts/build/lint.mk
+++ scripts/build/lint.mk
@@ -19,7 +19,7 @@
 GO_LINT = $(GO_PATH)/bin/golangci-lint
 
 linter:
-	$(GO_LINT) version || curl -sfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b $(GO_PATH)/bin v2.4.0
+	$(GO_LINT) version || curl -sfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b $(GO_PATH)/bin v2.12.2
 
 .PHONY: lint
 lint: linter generate
